cb4b0c55acd51c0035c2d0a7aec45797441ae55e
kahviakanellila <86312291+kahviakanellila@users.noreply.github.com>
2023-11-02 20:44:06 +0100

    update task3_L.cpp to finished version

--- task3_L.cpp
+++ task3_L.cpp
@@ -1,91 +1,154 @@
 // -------------------------------
 // Group: 13
-// Members: Charlotte Kauter, Robert Sitner, Artem Suttar
-// HU-Account names: kauterch, x, y
+// Members:
+//      Charlotte Kauter
+//      Robert Sitner
+//      Artem Suttar
+//HU-Account names:
+//      kauterch
+//      sitnerro
+//      suttarar
 // -------------------------------
 #include <iostream>
 #include <fstream>
 #include <sys/resource.h>
 #include <string>
 //added:
-#include <vector>
-#include <sstream>
-#include <cstdlib>
-#include <algorithm>
-#include <queue>
+#include <algorithm> //sort
+#include <queue>  // priority queue, vector
+
+//for more readability 
+typedef std::pair<int, int> pr; 
+
+
 
 void sort_external_file(const std::string &input_file_name, const std::string &output_file_name) { 
+    //-------------------------------------------------------------------------------------------------SORT
+
     //proof if the input file exists
     std::ifstream infile(input_file_name);
     if (infile.is_open()) {
-        //check if outfile exists
-        //if: erase
-        //if not: create
-        //"w": Creates an empty file for writing. 
-        //If a file with the same name already exists, its content is erased and the file is considered as a new empty file.
+
+        /*check if outfile exists
+            if: erase | if not: create
+            "w": Creates an empty file for writing. 
+            If a file with the same name already exists, its content is erased and the file is considered as a new empty file.
+        */
+
         FILE *outfile = fopen(output_file_name.c_str(), "w");
-        //------------------------------------------------------
-        //define some variables:
-
-        // Define a block size (in number of integers) to fit within 50 MB of memory.
-        const long long BLOCK_SIZE = 5e7/ sizeof(int);  // (47 megabytes to keep it below 50 MB memory)
-        //Block counter
-        int block_i = 1;
+        //------------------------------------------------------VARIABLES:
         
-        while (!infile.eof()) {
-            std::string line;
-            std::vector<int> buffer(BLOCK_SIZE);  //define the buffer with the max size
-            int i = 0;                            //index for buffer
+        const long long BLOCK_SIZE = 5e7/ sizeof(int);  // Define a block size (in number of integers) to fit within 50 MB of memory.
+        int block_i = 1;                                //Block counter
+        std::vector<std::ifstream> tempFiles;           //to store the temp files => better access
+        std::vector<std::string> str_tempFiles;         //to be able to delete them
+        std::string line;                               //to read line by line
 
+        while (infile >> line) { //to prevent reading last empty line & proof if end of file is reached
+            
+            std::vector<int> buffer(BLOCK_SIZE);        //define the buffer with the max size
+            int i = 0;                                  //index for buffer
 
+            
             //read the file line by line until the buffer limit is reached
             while (i < BLOCK_SIZE && std::getline(infile, line)) {
-                buffer[i] = atoi(line.c_str());
-                i++;    //update buffer location
+                if(line.empty()) continue;              //skip empty lines
+                else{
+                    buffer[i] = atoi(line.c_str());     // string to integer
+                    i++;                                //update buffer location
+                }
             }
+            
+            std::sort(buffer.begin(), buffer.end());    //sort the current buffer
 
-            //sort the current buffer
-            std::sort(buffer.begin(), buffer.end());            
+            //------------------------------------------sorted buffer to temp file
 
-            //sorted buffer to temp file---------------------------------------------------------------------------
-            std::vector<std::ifstream> input_files; //to store the temp files => better access
             std::string filename = "temp_buffer_" +std::to_string(block_i) + ".txt"; //create file name
-            //open in write mode and erase content
-            std::ofstream tempFile(filename, std::ios::out | std::ios::trunc);//create and open a temporary file
+                                                                               //open in write mode and erase content
+            std::ofstream tempFile(filename, std::ios::out | std::ios::trunc); //create and open a temporary file
 
+            //throw exception if file is not okay
             if (!tempFile.is_open()) {
             std::cerr << "Failed to open the temporary file." << std::endl;
             }
 
-            //i: max pos in buffer
-            for (int j = 0; j < i; j++) { 
-                tempFile << buffer[j] << '\n'; //one number per line
+            //write current buffer to a temp file
+            for (int j = 0; j < i; j++) {               //i: max pos in buffer
+                if (j == 0){
+                    tempFile << buffer[j];              //write first entry without a line break
+                }else{
+                    tempFile << '\n' << buffer[j];      //write each number as one line
+                }
+                
             }
             
-            tempFile.close(); //close the temp file after it
-            input_files.push_back(std::ifstream(filename)); //store temp file
+            tempFile.close();                             //close the temp file after it
+            tempFiles.push_back(std::ifstream(filename)); //store temp file
+            str_tempFiles.push_back(filename);            //store temp filenames as strings to delete them later
             block_i++; //update Block counter
             
         }
-        //close input file
-        infile.close();
+        
+        infile.close();                                   //close input file
+        //-------------------------------------------------------------------------------------------------MERGE
 
-        //have to save all first elements
+        //pq is empty
+        //we create a priority queue with pairs to store:
+        //1st: number
+        //2nd: index of temp file in vector
 
-        //std::priority_queue<int, std::vector<int>, 'ComparisonType> pq; //MinHeap
-
-        /*while (!pq.empty()){
-
-            int minValue = ;
-            outfile << minValue << "\n";
-        } 
+        /*        
+           In C++ if the element is in the form of pairs, 
+           then by default the priority of the elements is dependent upon the first element.
+           By default priority queues are max heaps. 
+           Therefore, we just have to use the priority queue of pairs with greater<> function object. 
         */
 
+        std::priority_queue<pr, std::vector<pr>, std::greater<pr>> pq; //stored as MinHeap
 
-        fclose(outfile);
+        //1st filling: take the first element of each temp file
+        for (int i = 0; i < tempFiles.size() ; i++){
+            std::ifstream& tempFile = tempFiles[i];
+            int element;
+            tempFile >> element;
+            pq.push({element ,i});
+        }
+        
+        //NOTES:
+        //Have to: .push() elements of buffer to pq
+        //top() : first element (greatest prio)
+        //pop() : erase first element
+
+        //keep going if priority queue is not empty
+        while (!pq.empty()){
+
+            int value;                                          //to store top element of pq
+            int minValue = pq.top().first;                      //number
+            int tempIndex = pq.top().second;                    //temp file index
+
+            pq.pop();                                           //erase min entry 
+
+            std::string num = std::to_string(minValue) + " \n"; //create a string of number with line break
+            fprintf(outfile, num.c_str());                      //write string to output file
+
+            
+            if(!tempFiles[tempIndex].eof()){                    //proof if tempfile has elements to continue
+               tempFiles[tempIndex] >> value;                   //take the next number of the temp file with the current min
+               pq.push({value, tempIndex});                     //add new number to priority queue
+            }else{
+                tempFiles[tempIndex].close();                   //close the temp file if the end is reached
+            }
+        } 
+
+        
+        for (int i = 0 ; i < str_tempFiles.size(); i++){       //delete all temp files after finishing the merge step
+             std::remove(str_tempFiles[i].c_str());
+        }
+
+        fclose(outfile);                                       //close output file when we're ready
         return;
-    //if not: throw exception
-    }else{
+    
+    }else{                                                     //throw exception if input file does not exist
         std::cerr<< "The input file does not exist\n";
     }
     
@@ -138,9 +201,9 @@ bool file_is_sorted(const std::string &file_name) {
 }
 
 int main() {
-    const std::string input_file_name = "../large_file.txt";
+    const std::string input_file_name = "./large_file.txt";
     //const std::string input_file_name = "../small_file.txt";
-    const std::string output_file_name = "../sorted_file.txt";
+    const std::string output_file_name = "./sorted_file.txt";
 
     float memory_size = get_memory_usage();
     sort_external_file(input_file_name, output_file_name);
